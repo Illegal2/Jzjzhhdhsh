03315af6e8718ffdfe3139f4bf669e3a72c63c41
Illegal2 <devrimdeniz.0990@gmail.com>
2025-02-24 06:29:07 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# BİT Logo
+echo -e "\033[1;32m"
+echo "##########################"
+echo "#        JTH BİT        #"
+echo "##########################"
+echo -e "\033[0m"
+
+# Linkler
+echo -e "\033[1;34m"
+echo "Linkler:"
+echo "1. Papara için: https://github.com/Illegal2/Jzjzhhdhsh/site/1/Site"
+echo "2. Mimecraft için: https://github.com/Illegal2/Jzjzhhdhsh/site/2/Site"
+echo -e "\033[0m"
+
+# Menü Başlığı
+echo -e "\033[1;33m"
+echo "========== Menü =========="
+echo -e "\033[0m"
+
+# Menü Elemanları
+echo "¥《1》=========♧"
+echo "¥《2》=========♧"
+echo "¥《~Pubg Mobil [UC]~3》=========♧"
+echo "¥《4》=========♧"
+echo "¥《5》=========♧"
+echo "¥《6》=========♧"
+echo "¥《7》=========♧"
+echo "¥《8》=========♧"
+echo "¥《9》=========♧"
+echo "¥《99》@Joker @Ege Cin @Kuralsız"
+echo -e "\033[1;32m"
+echo "₺DARK WEP₺"
+echo -e "\033[0m"
+
+# Yönlendirme
+echo "Siteye Yönlendiriliyorsunuz..."
+echo "Yönlendirilen link: https://t.me/JTH_chat"
+
+# GitHub Deposu Linki
+echo -e "\033[1;35m"
+echo "GitHub Deposu: https://github.com/Illegal2/Jzjzhhdhsh.git"
+echo -e "\033[0m"
